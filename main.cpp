24f1bd6fd2d8354a613766b818cbab2d75c8d3f9
LeoDarcy <baidurnt@163.com>
2019-12-26 16:28:32 +0800

    merge master

--- main.cpp
+++ main.cpp
@@ -3,11 +3,8 @@ using namespace std;
 int main()
 {
 	cout << "Try Git" << endl;
-<<<<<<< HEAD
 	cout <<" This is master b1" << endl;	
 	cout <<" I am branch b1" << endl;
-=======
 	cout <<" This is master a1" << endl;	
 	cout <<" I am branch a1" << endl;
->>>>>>> a1
 }
